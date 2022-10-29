138d53af28c89f39a89247e094ca8bb741c060af
Vila Juan Cruz <yunagosh7@gmail.com>
2022-10-29 20:58:44 -0300

    Estilos finalizados

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+const submit = document.getElementById("submit");
+const title = document
+
+
+
+
+
+
+
+
+
+
+
+let books = [
+    {
+        title: "IT",
+        author:"Stephen King",
+        pages:1503,
+        readit: true
+    },
+    {
+        title: "Valle de la Calma",
+        author:"Angel David Revilla",
+        pages:294,
+        readit: true
+    },
+    {
+        title: "Aves de Presa",
+        author:"Wilbur Smith",
+        pages:591,
+        readit: true
+    },
+    {
+        title:"Lobos de Calla",
+        author:"Stephen King",
+        pages: 816,
+        readit: false
+    }
+]
+
